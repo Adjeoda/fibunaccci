80287a937dafbe6fb4312917777d18e78f4300e2
Adzeoda <80847832+Adjeoda@users.noreply.github.com>
2022-04-22 03:14:42 +0000

    Update Billing.py

--- Billing.py
+++ Billing.py
@@ -1,23 +1,13 @@
-noRooms=int(input("enter the number of rooms in the house: "))
-roomPopulation=list("")
+n1=0
+ne=1
+n3=n1+n2
+nth=int(input("input the nth term"))
 
-count=1
-housePop=0
+count=0
 
-while count<=noRooms:
-    print("enter the number of occupants in room{}: ".format(count))
-    x=int(input(": "))
-    roomPopulation.append(x)
-    housePop+=x
-    count+=1
-print("there are {} people living here".format(housePop))
-
-#billing
-bill=float(input("enter the bill amount in floating digits eg'100.00'"))
-quota=bill/housePop
-print("the quota is {}" .format(quota))
-
-count2=0
-while count2<=len(roomPopulation)-1:
-    print("room {} pays {}GHc" .format(count2+1,roomPopulation[count2]*quota))
-    count2+=1
+while  count<nth:
+    n1=n2
+    n2=n3
+    n3=n1+n2
+    print(n3)
+    
